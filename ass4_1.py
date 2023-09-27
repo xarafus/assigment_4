666d34a8ce6492e3c4517d2b5f789cdfaa214d11
xarafus <125128286+xarafus@users.noreply.github.com>
2023-09-27 11:46:52 +0600

    Create ass4_1.py

---
+++ ass4_1.py
@@ -0,0 +1,23 @@
+def grade(score):
+    try:
+        if 0 <= score <= 100:
+            if 90 <= score <= 100:
+                print("Grade is A")
+            elif 80 <= score < 90: 
+                print("Grade is B")   
+            elif 70 <= score < 80: 
+                print("Grade is C")
+            elif 60 <= score < 70:
+                print("Grade is D")
+            else:
+                print("Grade is F")
+        else: 
+            print("Error, please enter numeric input between 0 and 100")
+    except ValueError:
+        print("Error, please enter numeric input between 0 and 100")
+try:
+    score = int(input('Enter the number: '))
+    mark = grade(score)
+    print(mark)
+except ValueError:
+    print("Error, please enter numeric input between 0 and 100")
